6440a12bf14a3ec82ca1acff2acccdb70804b040
Gregor Gilchrist <gregorgilchrist@gmail.com>
2023-04-04 20:50:20 +0200

    it is working

--- radio-server/player.js
+++ radio-server/player.js
@@ -8,14 +8,15 @@ const readableSong = fs.createReadStream("../songs/Out-of-Touch(128kbps).mp3");
 const throttle = new Throttle(128000 / 8);
 let _sinks = [];
 
-module.makeResponseSink = () => {
+const makeResponseSink = () => {
   const responseSink = PassThrough();
   _sinks.push(responseSink);
   console.log("make response sink");
+  console.log({ _sinks });
   return responseSink;
 };
 
-module.playSong = () => {
+const playSong = () => {
   readableSong.pipe(throttle).on("data", (chunk) => {
     console.log("play song");
     for (const writable of writables) {
@@ -23,3 +24,5 @@ module.playSong = () => {
     }
   });
 };
+
+module.exports = { makeResponseSink, playSong };
--- radio-server/server.js
+++ radio-server/server.js
@@ -1,12 +1,59 @@
 const http = require("http");
-const player = require("./player");
+const fs = require("fs");
+const { strictEqual } = require("assert");
+// const { makeResponseSink, playSong } = require("./player");
+
+// Function to stream audio data to the user
+function streamAudio(req, res) {
+  // Open the audio file
+  const filePath = "../songs/Out-of-Touch(128kbps).mp3";
+  const stat = fs.statSync(filePath);
+  console.log("stat: ", { stat });
+  const fileSize = stat.size;
+  const range = req.headers.range;
+  console.log("range: ", { range });
+
+  // Check if a range header was sent by the client
+  if (range) {
+    // Parse the range header
+    const parts = range.replace(/bytes=/, "").split("-");
+    console.log("range parts: ", { parts });
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+    const chunksize = end - start + 1;
+
+    // Set the response headers to send a partial content response
+    res.writeHead(206, {
+      "Content-Range": "bytes " + start + "-" + end + "/" + fileSize,
+      "Accept-Ranges": "bytes",
+      "Content-Length": chunksize,
+      "Content-Type": "audio/mpeg",
+    });
+
+    // Create a stream to read the audio file chunk by chunk and send it as a response
+    const fileStream = fs.createReadStream(filePath, { start, end });
+    fileStream.pipe(res);
+  } else {
+    // Set the response headers to send the full audio file
+    res.writeHead(200, {
+      "Content-Length": fileSize,
+      "Content-Type": "audio/mpeg",
+    });
+
+    // Create a stream to read the audio file and send it as a response
+    const fileStream = fs.createReadStream(filePath);
+    fileStream.pipe(res);
+  }
+}
 
 const server = http.createServer((request, response) => {
   if (request.method === "GET" && request.url === "/play") {
     console.log("got /play request!");
-    // const { id, responseSink } = player.makeResponseSink();
+
+    streamAudio(request, response);
+    // const { id, responseSink } = makeResponseSink();
     // request.app.sinkId = id;
-    // return h.response(responseSink).type("audio/mpeg");
+    // return response(responseSink).type("audio/mpeg");
   }
 });
 
